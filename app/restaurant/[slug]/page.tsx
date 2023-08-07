e3a11d205d1e1ec54da060a8818487783d195f23
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-08-07 16:13:50 +0500

    Placing universally used components in the Root Layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import NavBar from '../../components/NavBar';
 import Description from './components/Description';
 import Header from './components/Header';
 import Images from './components/Images';
@@ -10,44 +9,39 @@ import Title from './components/Title';
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <NavBar />
-        {/* NAVBAR */}
-        {/* HEADER */}
-        <Header />
-        {/* HEADER */}
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            {/* RESAURANT NAVBAR */}
-            <RestaurantNavBar />
-            {/* RESAURANT NAVBAR */}
-            {/* TITLE */}
-            <Title />
-            {/* TITLE */}
-            {/* RATING */}
-            <Rating />
-            {/* RATING */}
-            {/* DESCRIPTION */}
-            <Description />
-            {/* DESCRIPTION */}
-            {/* IMAGES */}
-            <Images />
-            {/* IMAGES */}
-            {/* REVIEWS */}
-            <Reviews />
-            {/* REVIEWS */}
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
+    <>
+      {/* HEADER */}
+      <Header />
+      {/* HEADER */}
+      {/* DESCRIPTION PORTION */}
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[70%] rounded p-3 shadow">
+          {/* RESAURANT NAVBAR */}
+          <RestaurantNavBar />
+          {/* RESAURANT NAVBAR */}
+          {/* TITLE */}
+          <Title />
+          {/* TITLE */}
+          {/* RATING */}
+          <Rating />
+          {/* RATING */}
+          {/* DESCRIPTION */}
+          <Description />
+          {/* DESCRIPTION */}
+          {/* IMAGES */}
+          <Images />
+          {/* IMAGES */}
+          {/* REVIEWS */}
+          <Reviews />
+          {/* REVIEWS */}
         </div>
-        {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */}{' '}
-        {/* RESERVATION
+        <div className="w-[27%] relative text-reg">
+          <ReservationCard />
+        </div>
+      </div>
+      {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */}{' '}
+      {/* RESERVATION
     CARD PORTION */}
-      </main>
-    </main>
+    </>
   );
 }
